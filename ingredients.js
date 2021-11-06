dc1bda28c1a9e88fb0175c0b66ce999e2eeb48c3
Zeinab Mohamed <zeinabtmohamed@gmail.com>
2021-11-06 22:36:08 +0000

    Iterating Over Arrays: Banana Bread

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+while (ingredients < 8) { 
+  console.log(ingredients)
+}
+
+for (const ingredient of ingredients){
+  console.log(ingredients)
+}
+
+for (let i = 0; i < something; I++) {
+  while(ingredients < 8){
+    console.log(ingredients.reverse)
+  }
+};
